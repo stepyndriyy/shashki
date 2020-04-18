87dcb61a997df1ed6817ab6ad71ee23c59e56515
stepan <stepyndriyy@gmail.com>
2020-04-18 20:13:07 +0300

    bad version0.1

--- gui.py
+++ gui.py
@@ -7,12 +7,15 @@ from src.ai import AI, PositionEvaluation
 from src.boardstate import BoardState
 
 
-def draw_board(screen: Surface, pos_x: int, pos_y: int, elem_size: int, board: BoardState):
+def draw_board(screen: Surface, pos_x: int, pos_y: int, elem_size: int, board: BoardState, GreenPlates : list):
     dark = (0, 0, 0)
     white = (200, 200, 200)
+    green = (0, 128, 0)
 
     for y, x in product(range(8), range(8)):
         color = white if (x + y) % 2 == 0 else dark
+        if (y, x) in GreenPlates:
+            color = green
         position = pos_x + x * elem_size, pos_y + y * elem_size, elem_size, elem_size
         pygame.draw.rect(screen, color, position)
 
@@ -20,7 +23,6 @@ def draw_board(screen: Surface, pos_x: int, pos_y: int, elem_size: int, board: B
 
         if figure == 0:
             continue
-
         if figure > 0:
             figure_color = 255, 255, 255
         else:
@@ -32,18 +34,20 @@ def draw_board(screen: Surface, pos_x: int, pos_y: int, elem_size: int, board: B
             r = 5
             negative_color = [255 - e for e in figure_color]
             pygame.draw.circle(screen, negative_color, (position[0] + elem_size // 2, position[1] + elem_size // 2), r)
-
+    
 
 def game_loop(screen: Surface, board: BoardState, ai: AI):
     grid_size = screen.get_size()[0] // 8
-
+    GreenPlates = []
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mouse_click_position = event.pos
+                mouse_click_position = event.pos 
+                old_x, old_y = [p // grid_size for p in mouse_click_position]
+                GreenPlates = board.get_possible_moves(old_y, old_x)
 
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 new_x, new_y = [p // grid_size for p in event.pos]
@@ -52,7 +56,8 @@ def game_loop(screen: Surface, board: BoardState, ai: AI):
                 new_board = board.do_move(old_x, old_y, new_x, new_y)
                 if new_board is not None:
                     board = new_board
-
+                GreenPlates = []
+            
             if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
                 x, y = [p // grid_size for p in event.pos]
                 board.board[y, x] = (board.board[y, x] + 1 + 2) % 5 - 2  # change figure
@@ -65,8 +70,19 @@ def game_loop(screen: Surface, board: BoardState, ai: AI):
                     new_board = ai.next_move(board)
                     if new_board is not None:
                         board = new_board
+        
 
-        draw_board(screen, 0, 0, grid_size, board)
+        draw_board(screen, 0, 0, grid_size, board, GreenPlates)
+        if board.is_game_finished:
+            font = pygame.font.Font(None, 75)
+            if board.get_winner == 1:
+                text = font.render("YOU WIN", True, (0, 255, 0))
+            else: 
+                text = font.render("YOU LOSE", True, (255, 0, 0))
+            x, y = map(int, screen.get_size())
+            
+            screen.blit(text, [x // 4, y // 2]) 
+        
         pygame.display.flip()
 
 
--- src/boardstate.py
+++ src/boardstate.py
@@ -1,3 +1,4 @@
+from itertools import product
 import numpy as np
 from typing import Optional, List
 
@@ -6,6 +7,7 @@ class BoardState:
     def __init__(self, board: np.ndarray, current_player: int = 1):
         self.board: np.ndarray = board
         self.current_player: int = current_player
+        # print(board)
 
     def inverted(self) -> 'BoardState':
         return BoardState(board=self.board[::-1, ::-1] * -1, current_player=self.current_player * -1)
@@ -13,34 +15,119 @@ class BoardState:
     def copy(self) -> 'BoardState':
         return BoardState(self.board.copy(), self.current_player)
 
+    def get_possible_moves(self, from_y, from_x) -> List['BoardState']:
+        if self.board[from_y, from_x] == 0: 
+            return []
+        
+        answer = list()
+        if self.board[from_y, from_x] == 1 and from_y - 1 >= 0:
+            for dx in range(-1, 2, 2):
+                to_y = from_y - 1
+                to_x =  from_x + dx
+                if to_x < 0 or to_x >= 8:
+                    continue
+                
+                #check if empty plate
+                if self.board[to_y, to_x] == 0:
+                    answer.append((to_y, to_x))
+                
+                #check if enemy on plate
+                if self.board[to_y, to_x] < 0:
+                    to_x += dx
+                    to_y += -1
+                    if to_x < 0 or to_x >= 8 or to_y < 0:
+                        continue
+                    if self.board[to_y, to_x] == 0:
+                        answer.append((to_y, to_x))
+  
+        
+        if self.board[from_y, from_x] == 2:
+            for dx in range(-1, 2, 2):
+                for dy in range(-1, 2, 2):    
+                    enemy_on_line = 0 # cnt of enemy on line
+                    to_x = from_x + dx
+                    to_y = from_y + dy
+                    while (to_x >= 0 and to_x < 8) and (to_y >=  0 and to_y < 8):
+                        if enemy_on_line > 1:
+                            break
+
+                        if self.board[to_y, to_x] > 0: # if we meet our piece
+                            break
+                    
+                        if self.board[to_y, to_x] < 0:
+                            enemy_on_line += 1
+
+                        if self.board[to_y, to_x] == 0:
+                            answer.append((to_y, to_x))
+                        
+                        to_x += dx
+                        to_y += dy
+    
+        return answer 
+
+
+    def delete_enemy_in_between(self, from_y, from_x, to_y, to_x):
+        dy = 1 if to_y > from_y else -1
+        dx = 1 if to_x > from_x else -1
+        cur_x = from_x + dx 
+        cur_y = from_y + dy
+        while cur_x * dx > from_x * dx and cur_x * dx < to_x * dx and cur_y * dy > from_y * dy and cur_y * dy < to_y * dy:            
+            if self.board[cur_y, cur_x] < 0:
+                self.board[cur_y, cur_x] = 0
+            cur_x += dx
+            cur_y += dy
+
+
     def do_move(self, from_x, from_y, to_x, to_y) -> Optional['BoardState']:
         """
         :return: new BoardState or None for invalid move
         """
+        #print(from_y, from_x, self.board[from_y, from_x])
         if from_x == to_x and from_y == to_y:
             return None #invalid move
 
         if (to_x + to_y) % 2 == 0:
             return None
 
-        # todo more validation here
+        if not (to_y, to_x) in BoardState.get_possible_moves(self, from_y, from_x):
+            return None
 
+        
         result = self.copy()
+        
+        result.delete_enemy_in_between(from_y, from_x, to_y, to_x)
+        
         result.board[to_y, to_x] = result.board[from_y, from_x]
         result.board[from_y, from_x] = 0
+        
+        if to_y == 0:
+            result.board[to_y, to_x] = 2
 
         return result
 
-    def get_possible_moves(self) -> List['BoardState']:
-        return [] # todo
+    def get_white_and_black_count(self) -> list:
+        white_count = 0
+        black_count = 0
+        for y, x in product(range(8), range(8)):
+            if self.board[y, x] > 0:
+                white_count += 1
+            if self.board[y, x] < 0:
+                black_count += 1
+        return [white_count, black_count]
 
     @property
     def is_game_finished(self) -> bool:
-        ... # todo
+        cnt = self.get_white_and_black_count()   
+        if cnt[0] == 0 or cnt[1] == 0:
+            return True
+        return False
 
     @property
     def get_winner(self) -> Optional[int]:
-        ... # todo
+        if not self.is_game_finished:
+            return None
+        cnt = self.get_white_and_black_count()
+        return (1 if cnt[1] == 0 else -1)
 
     @staticmethod
     def initial_state() -> 'BoardState':
